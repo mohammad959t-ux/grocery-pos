9e4dbe7520dc6993518aec186c55e4b23b494378
mohammad959t-ux <mohammad959t@gmail.com>
2025-08-27 18:51:45 +0300

    My recent changes

--- models/Sale.js
+++ models/Sale.js
@@ -1,11 +1,14 @@
+// models/Sale.js
 const mongoose = require('mongoose');
 
+// تفاصيل كل منتج ضمن عملية البيع
 const saleItemSchema = mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   quantity: { type: Number, required: true },
-  price: { type: Number, required: true } // سعر القطعة أو الكرتون عند البيع
+  price: { type: Number, required: true } // السعر الفعلي للقطعة أو الكرتون
 });
 
+// schema الرئيسي لعملية البيع
 const saleSchema = mongoose.Schema({
   items: [saleItemSchema],
   total: { type: Number, required: true },
