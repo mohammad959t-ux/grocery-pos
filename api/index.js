f9e50e414de6babbe31921503aa420934872dd24
mohammad959t-ux <mohammad959t@gmail.com>
2025-08-28 01:33:32 +0300

    My recent changes

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/products', require('./routes/productRoutes'));
-app.use('/sales', require('./routes/saleRoutes'));
-app.use('/analytics', require('./routes/analyticsRoutes'));
+app.use('/products', require('../routes/productRoutes'));
+app.use('/sales', require('../routes/saleRoutes'));
+app.use('/analytics', require('../routes/analyticsRoutes'));
 
 // Root
 app.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 // Connect to the database
 connectDB();
 
-// use the app from the api folder as a whole
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
